a179cd6026cc6be333ad87ed8503cc00ea9b75e2
widushan <114930601+widushan@users.noreply.github.com>
2025-12-15 19:34:52 +0530

    template commit
    Start to build templates

--- src/app.py
+++ src/app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import socket
 
 app = Flask(__name__)
+
+
+# Function to fetch host details
+def fetchDetails():
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    return str(host_name), str(host_ip)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status="UP"
+    )
+
+@app.route("/details")
+def details():
+    host_name, host_ip = fetchDetails()
+    return render_template("index.html", HOSTNAME=host_name , IP=host_ip)
 
 if __name__ == "__main__":
     app.run(debug=True)
