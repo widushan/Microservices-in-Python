60f2961153edb8607360c1e2e1fc677634d7ac56
widushan <114930601+widushan@users.noreply.github.com>
2025-12-16 19:21:51 +0530

    kube commit
    kubectl

--- src/app.py
+++ src/app.py
@@ -26,4 +26,4 @@ def details():
     return render_template("index.html", HOSTNAME=host_name , IP=host_ip)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
